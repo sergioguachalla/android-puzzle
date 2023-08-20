b5d6b3b5546a634f4ad3fb7ed20b1b8fd758e2c7
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-08-20 16:44:36 -0400

    Level one and home fragment added

--- app/src/main/java/com/example/puzzle/MainActivity.java
+++ app/src/main/java/com/example/puzzle/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.puzzle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    HomeFragment homeFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, homeFragment).commit();
+
     }
 }
